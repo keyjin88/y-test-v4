08129c017c78a39ef17b83cb8e414e6851ca2007
AV <keyjin88@gmail.com>
2025-06-02 16:09:04 +0300

    Initial commint

---
+++ src/main/java/ru/vavtech/ytestv4/YTestV4Application.java
@@ -0,0 +1,13 @@
+package ru.vavtech.ytestv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YTestV4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YTestV4Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vavtech/ytestv4/YTestV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vavtech.ytestv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YTestV4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
